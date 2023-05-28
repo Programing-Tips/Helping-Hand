29545ceb327805f6ccb1345cefc4731db95a89dd
Programing-Tips <134650714+Programing-Tips@users.noreply.github.com>
2023-05-28 19:19:41 +0200

    Update (obesenec).py

--- (obesenec).py
+++ (obesenec).py
@@ -5,25 +5,25 @@ from string import ascii_lowercase
 
 running = True
 
-
-word_list = ["pes",'macka','les','jez','mys','mamicka','tatko','zajac',]
+word_list = ["eagle", "python", "snake"]
 
 target = random.choice(word_list)
-guess = "*" * len(target)
-correct = 0
+guessed = {}.fromkeys(target, False)
 
+def get_mask():
+    global target, guessed
+    return ''.join([ch if guessed[ch] else '*' for ch in target])
 
-def canvas_onkey(event):
-    global running, guess, correct, target, c
-
-    if event.keysym == target[correct]:
-        g = list(guess)
-        g[correct] = target[correct]
-        guess = ''.join(g)
-        c.itemconfigure(text1, text=guess)
-        correct += 1
-
-    if correct == len(target):
+def on_key(event):
+    global running, guessed, target, c
+    if event.keysym not in guessed:
+        return                  # Incorrect guess
+    if guessed[event.keysym]:
+        return                  # We had already guessed this letter
+    guessed[event.keysym] = True
+    c.itemconfigure(text1, text=get_mask())
+    if False not in guessed.values():
+        c.unbind_all("<Key>")
         running = False
 
 
@@ -36,32 +36,28 @@ c.pack()
 x = random.randint(14, 486)
 y = 14
 
-text1 = c.create_text(x, y, text=guess, font='Arial, 14',)
+text1 = c.create_text(x, y, text=get_mask(), font='Arial, 14',)
 text2 = c.create_text(x, y, text=target, font='Arial, 14',  state='hidden')
 
-shown = False
-
-dy = 0
-
-while running:
-    dx = 0
-    dy += 5000 * TIMESTEP
-    dy *= TIMESTEP
+def on_update():
+    global c, x, y, running
+    dy = 60 * TIMESTEP
     y += dy
-    c.move(text1, dx, dy)
-    c.move(text2, dx, dy)
-    c.bind_all(f"<Key>", canvas_onkey)
+    c.move(text1, 0, dy)
+    c.move(text2, 0, dy)
 
 
-    if y > 486 and not shown:
-        shown = True
-        c.create_text(250, 225, text="Neuhádol si!", font='Arial, 14',)
+    if y > 486:
+        c.create_text(250, 225, text="NeuhÃ¡dol si!", font='Arial, 14',)
         c.itemconfigure(text2, state='normal')
         c.itemconfigure(text1, state='hidden')
         running = False
 
     c.update()
-    time.sleep(TIMESTEP)
+    if running:
+        c.after(int(TIMESTEP * 1000), on_update)
 
 
+c.bind_all("<Key>", on_key)
+on_update()
 c.mainloop()
